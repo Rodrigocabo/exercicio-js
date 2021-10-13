df86b02365e8a073e2fb6f7770f961c4d023608f
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-13 20:44:09 -0300

    exercicio

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var nome = "Rodrigo carvalho";
-var idade = 29;
-var idade2 = 10;
-var frase = "flamengo e o melhor do time do mundo";
+
+
+var lista =["maça","pera","laranja];
+lista.push("uva");
+console.log(lista);
+//var nome = "Rodrigo carvalho";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "flamengo e o melhor do time do mundo";
 ///alert(nome + " tem "+ idade + "anos");
 ///alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("flamengo","vasco"));
+///console.log(nome);
+///console.log(id+ idade2);
+///console.log(frase.replace("flamengo"+"vasco"));
