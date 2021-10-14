7b5dcd256792abf7465410388063866eca932b8a
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-13 21:07:35 -0300

    bytebank

---
+++ index.js
@@ -0,0 +1,10 @@
+ const cliente1nome ="Carlos";
+ const cliente1cpf = 11122233301;
+ const cliente1agencia = 1001;
+ const cliente1saldo = 0;
+
+ const cliente2nome = "keila";
+ const cliente2cpf = 1112222233301;
+ const cliente2agencia = 1001;
+ const cliente2saldo = 0;
+ console.log(cliente1nome);
