2ee5c354b2d38b36e0831f9c053cc22cc7f0d32e
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-13 21:30:40 -0300

    bytebank

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 class cliente{
     nome;
     cpf;
+    
+}
+
+class contacorrente{
     agencia;
     saldo;
+
 }
+
 const cliente1 = new cliente();
 const cliente2 = new cliente();
+
         cliente1.nome ="Carlos";
         cliente1.cpf = 11122233301;
-        cliente1.agencia = 1001;
-        cliente1.saldo = 0;
+       
 
         cliente2.nome = "keila";
         cliente2.cpf = 1112222233301;
-        cliente2.agencia = 1001;
-        cliente2.saldo = 0;
- console.log(cliente1);
+       
+const contacorrentecarlos = new contacorrente();
+contacorrentecarlos.saldo =0;
+contacorrentecarlos.agencia = 1001;
+ console.log(contacorrentecarlos.saldo);
+ contacorrentecarlos.saldo = 100;
+ console.log(contacorrentecarlos.saldo);  
+ let valorsacado= 200;
+ if(contacorrentecarlos.saldo >= valorsacado){
+    contacorrentecarlos -= valorsacado;
+ contacorrentecarlos.saldo -= valorsacado;                                                                    
+ }
+
+
+
+         console.log(cliente1);
+        console.log(cliente2);
