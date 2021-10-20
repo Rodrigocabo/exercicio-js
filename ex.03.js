d221a37960159e384c441d1afa229bf67623b9b5
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-19 21:40:07 -0300

    exercicio

--- ex.03.js
+++ ex.03.js
@@ -5,4 +5,11 @@ const num1 = 10; // number
 const num2 = 10.52
 let nomeAluno; // undefined = não aponta pra local nenhum na memoria
 const sobrenomeAluno = null; // nulo -> não aponta para local nenhum na memoria
-const boolean = true + false 
+const aprovado = true + false  // bolean = logico
+
+const a = [1,2];
+const b = a;
+console.logo(a, b);
+
+b.push(3);
+console.log (a, b);
