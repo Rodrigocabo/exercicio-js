161ab7b6321d4ee139d6ceeddb7f82eaff427d13
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-19 21:28:16 -0300

    ex

--- ex.03.js
+++ ex.03.js
@@ -1,6 +1,8 @@
 const nome = 'rodrigo'; // string
-const nome1 = 'rodrigo';
-const nome2 = 'rodrigo';
+const nome1 = 'carlos';
+const nome2 = 'keila';
 const num1 = 10; // number
 const num2 = 10.52
 let nomeAluno; // undefined = não aponta pra local nenhum na memoria
+const sobrenomeAluno = null; // nulo -> não aponta para local nenhum na memoria
+const boolean = true + false 
