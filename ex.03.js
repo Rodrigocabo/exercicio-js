812aa5328a8018008eb5d346fc7f27c916bd3e7c
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-19 21:20:37 -0300

    exercicio

---
+++ ex.03.js
@@ -0,0 +1,6 @@
+const nome = 'rodrigo'; // string
+const nome1 = 'rodrigo';
+const nome2 = 'rodrigo';
+const num1 = 10; // number
+const num2 = 10.52
+let nomeAluno; // undefined = não aponta pra local nenhum na memoria
