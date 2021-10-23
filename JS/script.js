99c514e306e8e06914c0d3cb8352453090b72eb4
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-22 22:08:59 -0300

    exercicio

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
-const num1 = prompt('digite um numero');
-const num2 = prompt('digite outro numero');
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite outro numero');
+
+
+num1 = Number(num1);
+num2 = Number(num2);
+console.log(num1 + num2);
 const resultado = num1 + num2;
-alert('o resultado foi'    +   20);
+alert('o resultado foi:'  +  resultado);
