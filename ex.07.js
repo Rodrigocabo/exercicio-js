17c700385bb9739650f9b586ce3e1cb578022f93
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-25 20:21:36 -0300

    ex07

--- ex.07.js
+++ ex.07.js
@@ -1,5 +1,11 @@
+// IEEE 754-200 PADRÃO
+
+
 let num1 =1;
 let num2 = 2.5;
 
-console.log(num1.tostring() + num2);
-console.log(typeof num1);
+//console.log(num1.tostring() + num2);
+//console.log(typeof num1);
+
+let temp = num1 * 'ola';
+console.log(Number.isNaN(temp));
