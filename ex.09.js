0fd64fc37d1670fba4d0a47690d35e74c9c8fc0a
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-11-01 14:23:35 -0300

    ex09

---
+++ ex.09.js
@@ -0,0 +1,11 @@
+let alunos = ['carlos', 'keila', 'rodrigo'];
+
+alunos = 123;
+
+console.log(typeof  alunos);
+console.log(alunos instanceof Array);
+
+delete alunos[1];
+console.log(alunos[1]);
+
+
